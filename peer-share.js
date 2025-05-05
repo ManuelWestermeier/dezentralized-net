0267d0225f9943cb2233c4b0072420d5124d4f88
ManuelWestermeier <westermeier111@gmail.com>
2025-05-05 16:39:25 +0200

    publish test

--- peer-share.js
+++ peer-share.js
@@ -3,8 +3,14 @@ import net from "net";
 const users = new Map();
 
 net.createServer((socket) => {
+    console.log(socket.localAddress);
+
     socket.on("data", data => {
-        const out = JSON.parse(data.toString("utf-8"));
-        if (out.fn == "");
+
+        // const out = JSON.parse(data.toString("utf-8"));
+
+        // if (out.fn == "") {
+
+        // }
     });
 }).listen(22_668);
