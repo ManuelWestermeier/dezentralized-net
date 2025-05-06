377f0ea3701969a375f826ed4385292f12f30e1f
ManuelWestermeier <westermeier111@gmail.com>
2025-05-06 23:26:22 +0200

    .

--- user.js
+++ user.js
@@ -1,7 +1,13 @@
 import net from "net";
 import getUserIPList from "./src/get-user-ip-list.js";
-import { log } from "console";
 
 const userList = await getUserIPList(["localhost", 22668]);
 
-userList.forEach(log)
+const connectChunk = Math.min(20, Math.floor(userList / 2));
+const connectIPS = userList.slice(
+    Math.floor(connectChunk * Math.random()), connectChunk
+).map(ipStr => ipStr.split(":").map(parseInt));
+
+for (const ip of connectIPS) {
+
+}
